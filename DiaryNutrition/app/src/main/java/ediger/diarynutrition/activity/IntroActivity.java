3cd3cf2b10a9c9eed5b00caf3cfd442199e7f3d4
Edigarev R.D <ediger.r@gmail.com>
2016-05-26 19:43:01 +0300

    Intro design v1

--- DiaryNutrition/app/src/main/java/ediger/diarynutrition/activity/IntroActivity.java
+++ DiaryNutrition/app/src/main/java/ediger/diarynutrition/activity/IntroActivity.java
@@ -4,34 +4,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
-import com.github.paolorotolo.appintro.AppIntro2;
+import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroViewPager;
 
+import ediger.diarynutrition.R;
 import ediger.diarynutrition.fragments.intro.FirstSlide;
 import ediger.diarynutrition.fragments.intro.SecondSlide;
 
 /**
  * Created by root on 12.05.16.
  */
-public class IntroActivity extends AppIntro2 {
+public class IntroActivity extends AppIntro {
 
 
     @Override
     public void init(@Nullable Bundle savedInstanceState) {
 
+        showSkipButton(false);
+        setBarColor(getColor(R.color.intro_bar));
+        setIndicatorColor(getColor(R.color.intro_dark),
+                getColor(R.color.intro_light));
+        setColorDoneText(getColor(R.color.intro_dark));
+        setImageNextButton(getDrawable(R.drawable.ic_next_black));
+
+        setDoneText(getString(R.string.intro_done));
+
+
         addSlide(new FirstSlide());
         addSlide(new SecondSlide());
     }
 
-
     @Override
-    public void onDonePressed() {
-        finish();
+    public void onSkipPressed() {
+
     }
 
     @Override
     public void onNextPressed() {
 
+    }
+
+    @Override
+    public void onDonePressed() {
+        finish();
     }
 
     @Override
