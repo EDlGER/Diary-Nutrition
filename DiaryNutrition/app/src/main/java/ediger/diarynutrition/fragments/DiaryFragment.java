f469fe0d154a2b0b4b64d8c25e7a9fcc774e3704
Roman Edigarev <ediger.r@gmail.com>
2019-01-16 17:06:55 +0300

    Release 1.2.5(27)

--- DiaryNutrition/app/src/main/java/ediger/diarynutrition/fragments/DiaryFragment.java
+++ DiaryNutrition/app/src/main/java/ediger/diarynutrition/fragments/DiaryFragment.java
@@ -1,20 +1,16 @@
 package ediger.diarynutrition.fragments;
 
 
-import android.app.Activity;
 import android.content.Context;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.os.Build;
 import android.preference.PreferenceManager;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
