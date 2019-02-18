7371e03894ae3ce50e7de96fec1bd784b072474d
Roman Edigarev <ediger.r@gmail.com>
2019-02-18 15:52:13 +0300

    Create room entities

---
+++ DiaryNutrition/app/src/main/java/ediger/diarynutrition/data/Food.java
@@ -0,0 +1,97 @@
+package ediger.diarynutrition.data;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "food")
+public class Food {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "_id")
+    private int id;
+
+    @ColumnInfo(name = "food_name")
+    private String foodName;
+
+    private float cal;
+
+    private float prot;
+
+    private float fat;
+
+    private float carbo;
+
+    private int favor;
+
+    private int usr;
+
+    public Food(String foodName, float cal, float prot, float fat, float carbo) {
+        this.foodName = foodName;
+        this.cal = cal;
+        this.prot = prot;
+        this.fat = fat;
+        this.carbo = carbo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+
+    public float getCal() {
+        return cal;
+    }
+
+    public void setCal(float cal) {
+        this.cal = cal;
+    }
+
+    public float getProt() {
+        return prot;
+    }
+
+    public void setProt(float prot) {
+        this.prot = prot;
+    }
+
+    public float getFat() {
+        return fat;
+    }
+
+    public void setFat(float fat) {
+        this.fat = fat;
+    }
+
+    public float getCarbo() {
+        return carbo;
+    }
+
+    public void setCarbo(float carbo) {
+        this.carbo = carbo;
+    }
+
+    public int getFavor() {
+        return favor;
+    }
+
+    public void setFavor(int favor) {
+        this.favor = favor;
+    }
+
+    public int getUsr() {
+        return usr;
+    }
+
+    public void setUsr(int usr) {
+        this.usr = usr;
+    }
+}
---
+++ DiaryNutrition/app/src/main/java/ediger/diarynutrition/data/Meal.java
@@ -0,0 +1,27 @@
+package ediger.diarynutrition.data;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "meal")
+public class Meal {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "_id")
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
