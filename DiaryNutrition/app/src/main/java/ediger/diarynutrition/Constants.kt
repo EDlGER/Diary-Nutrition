c06c5a8f7e79835dfad1001f33d2531233b6ac1f
Roman Edigarev <its.ediger@gmail.com>
2022-10-27 15:13:52 +0300

    Define keys for PreferenceDataStore

--- DiaryNutrition/app/src/main/java/ediger/diarynutrition/Constants.kt
+++ DiaryNutrition/app/src/main/java/ediger/diarynutrition/Constants.kt
@@ -39,6 +39,7 @@ const val KEY_PROGRAM_CAL = "program_calories"
 const val KEY_PROGRAM_PROT = "program_prot"
 const val KEY_PROGRAM_FAT = "program_fat"
 const val KEY_PROGRAM_CARBO = "program_carbo"
+
 //Int
 const val KEY_ONLINE_DB_VERSION = "online_db_version"
 const val KEY_LOCAL_DB_VERSION = "local_db_version"
@@ -46,20 +47,21 @@ const val KEY_PROGRAM_PROT_PERCENT = "prot_pers"
 const val KEY_PROGRAM_FAT_PERCENT = "fat_percs"
 const val KEY_PROGRAM_CARBO_PERCENT = "carbo_pers"
 const val KEY_PROGRAM_WATER = "water"
-const val KEY_PREF_UI_WATER_CARD = "water_card"
 const val PREF_WATER_SERVING_1 = "water_serving_1"
 const val PREF_WATER_SERVING_2 = "water_serving_2"
 const val PREF_WATER_SERVING_3 = "water_serving_3"
 const val PREF_WATER_SERVING_4 = "water_serving_4"
-const val PREF_FILE_PREMIUM = "premium_data"
-const val PREF_PREMIUM = "premium"
 const val KEY_USER_FOOD_CONSTRAINT = "user_food_constraint"
+
+//Boolean
 const val KEY_FIRST_RUN = "first_run"
+const val KEY_MEALS_USER_HIDE = "meals_user_hide"
+const val KEY_PREF_UI_WATER_CARD = "water_card"
+const val PREF_PREMIUM = "premium"
+
+const val PREF_FILE_PREMIUM = "premium_data"
 const val ARG_EXPANSION_LEFT_OFFSET = "arg_left_offset"
 const val ARG_EXPANSION_TOP_OFFSET = "arg_top_offset"
 const val ARG_EXPANSION_VIEW_WIDTH = "arg_view_width"
 const val ARG_EXPANSION_VIEW_HEIGHT = "arg_view_height"
-
-//Boolean
-const val KEY_MEALS_USER_HIDE = "meals_user_hide"
 
